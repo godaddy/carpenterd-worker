f543f9faafdab94d4ef9d23c3c0c06ee0b828e22
mswaagman <mswaagman@godaddy.com>
2019-11-12 11:50:28 +0100

    [major] use dynamodb powered models

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const nsq = require('./preboot/nsq');
 const http = require('./preboot/http');
 const dirs = require('./preboot/dirs');
 const config = require('./preboot/config');
-const datastar = require('./preboot/datastar');
+const database = require('./preboot/database');
 const log = require('./preboot/log');
 
 module.exports = function carpenter(app) {
@@ -13,7 +13,7 @@ module.exports = function carpenter(app) {
     .use(dirs)
     .use(http)
     .use(nsq)
-    .use(datastar);
+    .use(database);
 };
 
 module.exports.worker = require('./worker');
